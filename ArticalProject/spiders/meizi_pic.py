440ded765e595c3f92fe9d43a07049d8ae303564
152056208 <31475416+152056208@users.noreply.github.com>
2018-08-29 20:59:22 +0800

    新增爬取妹子图片功能，速度有点慢，一分钟20张图片。

---
+++ ArticalProject/spiders/meizi_pic.py
@@ -0,0 +1,32 @@
+#-*-coding:utf8-*-
+#author : Lenovo
+#date: 2018/8/29
+import scrapy
+import re
+from scrapy.http import Request
+from urllib import parse
+from ArticalProject.items import MeiziItem
+from ArticalProject.utls.common import get_md5
+
+
+class MeiziSpider(scrapy.Spider):
+    name = 'meizi'
+    allowed_domains = ['www.meizitu.com']
+    start_urls = ['http://www.meizitu.com/a/more_1.html']
+
+
+    def parse(self,response):
+        pic_urls=response.css('.wp-item .tit a::attr(href)').extract()
+        for one in pic_urls:
+            yield Request(url=one,callback=self.parse_detail)
+        nex_url='http://www.meizitu.com/a/more_{0}.html'
+        #提取下一页并进行下载
+        for i in range(2,16):  #共有
+            print(123)
+            yield Request(url=nex_url.format(i),callback=self.parse)
+    def parse_detail(self,response):
+        pic_urls=response.css('#picture img::attr(src)').extract()
+        for one in pic_urls:
+            meizi=MeiziItem()
+            meizi['image_url']=[one]
+            yield meizi
